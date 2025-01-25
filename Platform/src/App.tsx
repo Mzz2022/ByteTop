79ad66a9741a717372b080846d36d6a743821569
mrlonely <mrlonely1226@163.com>
2025-01-26 01:20:45 +0800

    fix: fix platform

--- Platform/src/App.tsx
+++ Platform/src/App.tsx
@@ -1,19 +1,9 @@
-import type { NavigateOptions } from "react-router-dom";
-
-declare module "@react-types/shared" {
-  interface RouterConfig {
-    routerOptions: NavigateOptions;
-  }
-}
+import { useRoutes } from "react-router-dom";
+import routes from "./router";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center bg-blend-color">
-        欢迎来到ByteTop
-      </h1>
-    </>
-  );
+  const element = useRoutes(routes);
+  return element;
 }
 
 export default App;
