6242bb04a685b51480ef9d8294ede7d1f8925023
LXincheng <xincheng0116@gmail.com>
2025-02-03 23:33:39 +0800

    feat: 引入swagger

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 设置swagger文档
+  const config = new DocumentBuilder()
+    .setTitle('ByteTop数据服务')   
+    .setDescription('ByteTop数据服务接口文档')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
